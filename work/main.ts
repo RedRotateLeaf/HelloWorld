ccbd33748c5b87c332390e54c0f9cd32c2c96a30
USER-20140919IP\Administrator <2280333506@qq.com>
2016-09-19 21:08:09 +0800

    try again

---
+++ work/main.ts
@@ -0,0 +1,19 @@
+var line = 3;
+Paint(3);
+function Paint(n) {
+    var i=0;
+    for(i=0;i<n;i++) {
+        var front = "";
+        var kong = " "; 
+        for(var j=0;j<n-1-i;j++) {
+            front = front + kong;
+        }
+        var star = "*";
+        var starx = "";
+        for(var k=0;k<2*i+1;k++) {
+            starx = starx + star;
+        }
+        console.log(front+starx);
+
+    }
+}
